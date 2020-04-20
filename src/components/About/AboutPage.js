e618164310b51f5926d57407667d5710d560f5ac
nikzayn <nikhilvaidyar1997@gmail.com>
2020-04-20 10:56:19 +0530

    About Page Component --added

---
+++ src/components/About/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Simple React-Redux biolerplatr to setup any app</p>
+        </div>
+    )
+}
+export default AboutPage;
