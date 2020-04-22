833874b0b3b135eb5838bf4c9b15c541932d203c
nikzayn <nikhilvaidyar1997@gmail.com>
2020-04-22 23:41:31 +0530

    redux installed on root component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from './components/App';
+import configureStore from './redux/configureStore';
+import rootReducers from './redux/reducers';
+
 import './index.css';
 
+const store = configureStore(rootReducers);
 
 render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   , document.getElementById("app"));
